7a58ae374a4c7e295c5bc7b2f4314d6338ffbaa6
Sergei <suz2k@list.ru>
2024-06-22 21:40:05 +0400

    stage

--- install_wordpress/deploy.sh
+++ install_wordpress/deploy.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 set -xe
-export WORDPRESS_DB_HOST=${WORDPRESS_DB_HOST}
-export WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD}
+sudo usermod -aG docker gitlab-runner
+sudo chmod 666 /var/run/docker.sock
+export MYSQL_DATABASE=${WORDPRESS_DB_NAME}
+export MYSQL_USER=${WORDPRESS_DB_USER}
+export MYSQL_PASSWORD=${WORDPRESS_DB_PASSWORD}
 export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+export WORDPRESS_DB_USER=${WORDPRESS_DB_USER}
+export WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD}
 export WORDPRESS_DB_NAME=${WORDPRESS_DB_NAME}
-sudo docker-compose -f ~/docker-compose.yaml up -d
+docker-compose -f /home/gitlab-runner/docker-compose.yaml up -d
