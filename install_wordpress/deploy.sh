fa147cb1672d5f472ba1fab02ec58fd259c4bb0b
Sergei <suz2k@list.ru>
2024-06-22 19:56:56 +0400

    try to install wordpress stage

---
+++ install_wordpress/deploy.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+set -xe
+export WORDPRESS_DB_HOST=${WORDPRESS_DB_HOST}
+export WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD}
+export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+export WORDPRESS_DB_NAME=${WORDPRESS_DB_NAME}
+docker-compose -f ~/docker-compose.yaml up -d
